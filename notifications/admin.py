eb2c36b0343f8d540a564d299ff4514b07bf5694
spkuehl <skuehlhorn@gmail.com>
2021-02-19 21:30:37 -0600

    Add Notification model.

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Notification
 
-# Register your models here.
+
+class NotificationAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Notification, NotificationAdmin)
--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
+from goals.models import Goal
+from multiselectfield import MultiSelectField
 
-# Create your models here.
+
+FREQUENCY = (
+                ('daily', 'Daily'),
+                ('weekly', 'Weekly'),
+                ('monthly', 'Monthly'),
+                ('quarterly', 'Quarterly'),
+                ('half_yearly', 'Half Yearly'),
+                ('yearly', 'Yearly')
+            )
+
+DAY_OF_THE_WEEK = (
+                ('mon', 'Monday'),
+                ('tues', 'Tuesday'),
+                ('wed', 'Wednesday'),
+                ('thurs', 'Thursday'),
+                ('fri', 'Friday'),
+                ('sat', 'Saturday'),
+                ('sun', 'Sunday')
+            )
+
+class Notification(models.Model):
+    goal = models.ForeignKey(Goal, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    frequency = MultiSelectField(choices=FREQUENCY, null=True, blank=True)
+    day_of_the_week = MultiSelectField(choices=DAY_OF_THE_WEEK, null=True, blank=True)
+    time_to_notify = models.TimeField(null=True, blank=True)
+    active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '{}: {}'.format(self.goal, self.active)
